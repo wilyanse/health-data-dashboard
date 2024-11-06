c4b4d34e2473ea078ffd78dc06daa2ef8514d238
wilyanse <willjose51@gmail.com>
2024-11-06 15:00:36 +0800

    chore: reroute renamed pages

---
+++ dashboard-frontend/src/pages/GeneralStats.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <WeightChart />
+  </q-page>
+</template>
+
+<script setup>
+import WeightChart from '../components/WeightChart.vue'
+defineOptions({
+  name: 'GeneralStats',
+});
+</script>
