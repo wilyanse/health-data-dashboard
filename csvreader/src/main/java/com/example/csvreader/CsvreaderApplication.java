1cb4bb07864f8ef3070586d8d077942f889d53bc
wilyanse <willjose51@gmail.com>
2024-11-07 13:34:22 +0800

    feat(backend): add CORS config for dynamically created APIs

--- csvreader/src/main/java/com/example/csvreader/CsvreaderApplication.java
+++ csvreader/src/main/java/com/example/csvreader/CsvreaderApplication.java
@@ -2,25 +2,13 @@ package com.example.csvreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.csvreader")  // Ensure base package includes 'com.example.demo.config'
 public class CsvreaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsvreaderApplication.class, args);
-	}
-
-		@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-		@Override
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/weightdata").allowedOrigins("http://localhost:8080");
-		}
-		};
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CsvreaderApplication.class, args);
+    }
 }
