9d9891d8ebd0060a0ff078b4287dbbf79dba3f13
wilyanse <willjose51@gmail.com>
2024-11-08 11:10:05 +0800

    feat(backend): create new app for connecting to database

---
+++ mysqlconnector/src/main/java/com/backend/mysqlconnector/MysqlconnectorApplication.java
@@ -0,0 +1,13 @@
+package com.backend.mysqlconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlconnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlconnectorApplication.class, args);
+	}
+
+}
---
+++ mysqlconnector/src/test/java/com/backend/mysqlconnector/MysqlconnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.mysqlconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlconnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
