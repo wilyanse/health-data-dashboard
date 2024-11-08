364881da23ce9d788693a113432c4ac41cbfff39
wilyanse <willjose51@gmail.com>
2024-11-08 12:31:39 +0800

    feat(backend): create new mysqlconnector project

--- mysqlconnector/src/main/java/com/example/mysqlconnector/MysqlconnectorApplication.java
+++ mysqlconnector/src/main/java/com/example/mysqlconnector/MysqlconnectorApplication.java
@@ -1,4 +1,4 @@
-package com.backend.mysqlconnector;
+package com.example.mysqlconnector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- mysqlconnector/src/test/java/com/example/mysqlconnector/MysqlconnectorApplicationTests.java
+++ mysqlconnector/src/test/java/com/example/mysqlconnector/MysqlconnectorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.backend.mysqlconnector;
+package com.example.mysqlconnector;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
