a558867ee451cad035d3e846e30b7d56347483d8
wilyanse <willjose51@gmail.com>
2024-11-11 13:18:38 +0800

    chore(frontend): refactor GeneralStats page to use CompositionAPI

---
+++ dashboard-frontend/src/components/StatCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Test</h1>
+</template>
+
+<script>
+export default {
+    components: [],
+    methods: {
+        // store functions that involve reloading here
+    },
+    computed: {
+        // store funcitons that involve changing independent events here
+    },
+    setup() {
+        // 
+    },
+    data() {
+    },
+}
+</script>
--- dashboard-frontend/src/pages/GeneralStats.vue
+++ dashboard-frontend/src/pages/GeneralStats.vue
@@ -1,32 +1,27 @@
 <template>
   <div>
-    <h1 v-if="currentWeight !== null">Current Weight: {{ this.currentWeight }} kg</h1>
+    <h1 v-if="currentWeight !== null">Current Weight: {{ currentWeight }} kg</h1>
     <h1 v-else>Loading weight data...</h1>
     
     <WeightChart />
   </div>
 </template>
 
-<script>
-import { ref } from 'vue';
+<script setup>
+import { ref, onMounted } from 'vue';
 import WeightChart from '../components/WeightChart.vue';
 import apiService from '../services/ApiService.js';
 
-export default {
-  components: { WeightChart },
-  data () {
-    return {
-      currentWeight: ref(null),
-    }
-  },
-  mounted () {
-    this.loadCurrentWeight();
-    console.log('this ', this.currentWeight);
-  },
-  methods: {
-    async loadCurrentWeight() {
-      this.currentWeight = await apiService.getCurrentWeight();
-    }
-  },
-};
+// Declare reactive state variable
+const currentWeight = ref(null);
+
+// Define async function to load current weight
+async function loadCurrentWeight() {
+  currentWeight.value = await apiService.getCurrentWeight();
+}
+
+// Call loadCurrentWeight when the component is mounted
+onMounted(() => {
+  loadCurrentWeight();
+});
 </script>
