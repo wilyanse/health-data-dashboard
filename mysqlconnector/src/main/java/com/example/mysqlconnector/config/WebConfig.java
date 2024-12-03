d8214c99778944daf63173b2b0fa9ff34b798f2c
wilyanse <willjose51@gmail.com>
2024-12-03 14:01:42 +0800

    chore(devops): modify files to accommodate available ports

--- mysqlconnector/src/main/java/com/example/mysqlconnector/config/WebConfig.java
+++ mysqlconnector/src/main/java/com/example/mysqlconnector/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9000") // Adjust as needed
+                .allowedOrigins("http://localhost:9001", "http://localhost:9000", "http://localhost:7000") // Adjust as needed
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow OPTIONS method
                 .allowedHeaders("*")
                 .allowCredentials(true)
